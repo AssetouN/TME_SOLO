07d94e593aa6eedf6ec5efe0aeb3b467e9667721
Niambele Assetou <assetou.niambele@gmail.com>
2019-04-08 19:14:15 +0200

    q3

---
+++ volleyball_question3.py
@@ -0,0 +1,58 @@
+from soccersimulator import Strategy, SoccerAction, Vector2D, SoccerTeam
+from soccersimulator import VolleySimulation, volley_show_simu
+from tools import * 
+import math
+from random import randint
+GAME_WIDTH = 180
+GAME_HEIGHT = 90
+
+class Attaque(Strategy):
+    def __init__(self):
+        Strategy.__init__(self, "Fonceur")
+
+    def compute_strategy(self, state, id_team, id_player):
+        fct = SuperState(state, id_team, id_player)
+        if(fct.peut_frapper()):
+            if(fct.can_att):
+                if(fct.player.distance(fct.ennemi_proche())< 10):
+                    if(fct.ennemi_proche().y > GAME_HEIGHT/2):
+                        return fct.shoot( Vector2D((2-id_team)*GAME_WIDTH +8*(-1)**id_team ,random.randint(10,30) ) )
+                    else:
+                        return fct.shoot( Vector2D((2-id_team)*GAME_WIDTH +8*(-1)**id_team ,random.randint(55,75) ) )
+                else:
+                    return fct.shoot_but()
+            else:
+                return fct.pousse_ball()
+        else:
+            return fct.aller_vect
+        
+        
+        
+class Defense(Strategy):
+    def __init__(self):
+        Strategy.__init__(self, "Fonceur")
+
+    def compute_strategy(self, state, id_team, id_player):
+        fct = SuperState(state, id_team, id_player)
+        if fct.peut_frapper() :
+          return fct.degagement()
+        if fct.can_def :
+            return fct.aller_vect
+        return fct.aller(fct.position_defenseur)
+        
+        
+team1 = SoccerTeam(name="Team 1")
+team2 = SoccerTeam(name="Team 2")
+
+# Add players
+team2.add("kiwi",Defense()) 
+team1.add("kiwi",Attaque()) 
+
+
+
+
+
+simu = VolleySimulation(team1, team2)
+
+# Simulate and display the match
+volley_show_simu(simu)
